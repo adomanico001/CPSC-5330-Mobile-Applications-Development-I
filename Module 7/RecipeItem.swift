6115632e5a7bb9bb6d25f175c609b472a8a43a5b
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-12-01 19:15:32 -0500

    Create RecipeItem.swift

---
+++ Module 7/RecipeItem.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeItem.swift
+//  Module7Assignment
+//
+//  Created by Addie Domanico on 11/30/24.
+//
+
+import Foundation
+
+struct RecipeItem {
+    var recipe: String
+
+    init(_ recipe: String) {
+        self.recipe = recipe
+    }
+}
